cf59c81f1883af8d15e2e7f7310abcf26e0bb396
kmalhotra18 <malhotra.kunal@gmail.com>
2025-05-25 16:25:44 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -17,8 +17,6 @@ def hello() -> str:
     city, region, country = data['city'], data['region'], data['country']
     return f"Hello from {city}, {region}, {country}!!"
 
-# New - added thanks to student Tue H.!
-
 @app.function(image=image, region="eu")
 def hello_europe() -> str:
     import requests
